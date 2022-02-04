5c25cc098a033513770bd056d54646249d3c1b57
Rodricasares <rodrigocantudo@gmail.com>
2022-02-04 16:40:28 +0000

    Random poker card lista para corregir ;)

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".box").classList.add(randomSuit());
+  document.querySelector(".center").innerHTML = randomCard();
+};
+let randomCard = () => {
+  let num = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let random = Math.floor(Math.random() * num.length);
+  return num[random];
+};
+
+let randomSuit = () => {
+  let suit = ["spades", "clubs", "hearts", "diams"];
+  let random = Math.floor(Math.random() * suit.length);
+  return suit[random];
 };
